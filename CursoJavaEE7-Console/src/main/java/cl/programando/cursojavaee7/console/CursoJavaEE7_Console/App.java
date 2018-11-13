17a49765108ab7f85c99ecbdc1ab6d6e0ff14914
Francisco Arias Reyes <francisco.arias@programando.cl>
2018-11-13 00:03:51 -0300

    commit

---
+++ CursoJavaEE7-Console/src/main/java/cl/programando/cursojavaee7/console/CursoJavaEE7_Console/App.java
@@ -0,0 +1,13 @@
+package cl.programando.cursojavaee7.console.CursoJavaEE7_Console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
