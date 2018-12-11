68accc8b81efbe1f9f0313f3d59adc0bc847feb3
Francisco Arias Reyes <francisco.arias@programando.cl>
2018-12-11 19:58:39 -0300

    commit

---
+++ CursoJavaEE7-Struts/src/main/java/cl/programando/cursojavaee7/struts/TestAction.java
@@ -0,0 +1,37 @@
+package cl.programando.cursojavaee7.struts;
+
+import java.util.Date;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class TestAction extends ActionSupport 
+{
+	
+	private String name;
+    private Date nowDate;
+
+    @Override
+    public void validate(){
+        if (name==null || name.length()==0)
+            addActionError(getText("error.enter.message"));
+    }
+
+    @Override
+    public String execute() throws Exception {
+        nowDate = new Date();
+        return ActionSupport.SUCCESS;
+    }
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getNowDate() {
+        return nowDate;
+    }
+}
